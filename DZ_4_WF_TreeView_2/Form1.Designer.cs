180d1086512e83c7abac54ea3dbd8e3e546e7a92
Vetal <vvvv@vvvv.vv>
2024-01-13 14:32:06 +0300

    add TreeView_2

---
+++ DZ_4_WF_TreeView_2/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DZ_4_WF_TreeView_2
+{
+	partial class Form_MyTreeViewExplorer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.treeView_Explorer = new System.Windows.Forms.TreeView();
+			this.SuspendLayout();
+			// 
+			// treeView_Explorer
+			// 
+			this.treeView_Explorer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.treeView_Explorer.Location = new System.Drawing.Point(0, 0);
+			this.treeView_Explorer.Name = "treeView_Explorer";
+			this.treeView_Explorer.Size = new System.Drawing.Size(800, 450);
+			this.treeView_Explorer.TabIndex = 0;
+			this.treeView_Explorer.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView_Explorer_NodeMouseClick);
+			// 
+			// Form_MyTreeViewExplorer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.treeView_Explorer);
+			this.Name = "Form_MyTreeViewExplorer";
+			this.Text = "TreeView_Проводник_2(динамическая подгрузка открываемых node)";
+			this.Load += new System.EventHandler(this.Form_MyTreeViewExplorer_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TreeView treeView_Explorer;
+	}
+}
+
---
+++ DZ_4_WF_TreeView_2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZ_4_WF_TreeView_2
+{
+	public partial class Form_MyTreeViewExplorer : Form
+	{
+		DirectoryInfo currentDirectory;
+		public Form_MyTreeViewExplorer()
+		{
+			InitializeComponent();
+			//Создаенм объект для работы с каталогами
+			currentDirectory = new DirectoryInfo(@"C://");
+		}
+		//Form1_........Load(object sender, EventArgs e) - стандартная функция выполняющаяся при создании формы 
+		private void Form_MyTreeViewExplorer_Load(object sender, EventArgs e)
+		{
+			//Создаю массив объектов внутренних поддиректорий и получаю данные о поддерикториях из текущего каталога
+			DirectoryInfo[] subDirectories = currentDirectory.GetDirectories();
+			//Добавляю в ноду treeview название директорий
+			foreach (DirectoryInfo subDirectory in subDirectories)
+			{
+				treeView_Explorer.Nodes.Add(subDirectory.Name);
+			}
+		}
+		void loadDirectories(object sender, TreeNodeMouseClickEventArgs e)
+		{
+			currentDirectory = new DirectoryInfo($"C:/{e.Node.FullPath}");
+			DirectoryInfo[] subDirectories = currentDirectory.GetDirectories();
+			//Очищаю данные в открываемой ноде
+			e.Node.Nodes.Clear();
+			foreach (var item in subDirectories)
+			{
+				e.Node.Nodes.Add(item.Name);
+			}
+		}
+
+		//Обработка стандартного события - одинарный клик мышкой по ноде
+		private void treeView_Explorer_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+		{
+			try
+			{
+				loadDirectories(sender, e);
+				//Переключает узел дерева либо в развернутое либо в свернутое состояние
+				e.Node.Toggle();
+			}
+			catch (Exception)
+			{
+				MessageBox.Show($"Ошибка чтения каталога {e.Node.FullPath}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+
+
+
+		}
+	}
+}
